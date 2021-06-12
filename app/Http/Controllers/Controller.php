f907d21ee82603b1b9b2987850a6bb06a15559cd
chrisdavid32 <chrisdavid3271@gmail.com>
2021-06-12 09:07:26 +0100

    WIP

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,50 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    //Success status response
+    public function success($message)
+    {
+        $response = [
+            "message" => "success",
+            "data" =>  $message
+        ];
+        return response()->json($response, 200);
+    }
+
+    //Created status response
+    public function created($message)
+    {
+        $response = [
+            "message" => "created",
+            "data" =>  $message
+        ];
+        return response()->json($response, 201);
+    }
+
+    //Fail resquest status response
+    public function badrequest($message)
+    {
+        $response = [
+            "message" => "bad request",
+            "data" => ['error' => $message]
+        ];
+        return response()->json($response, 400);
+    }
+
+    //Not found status response
+    public function notfound($message)
+    {
+        $response = [
+            "message" => "not found",
+            "data" => ['error' => $message]
+        ];
+        return response()->json($response, 404);
+    }
+
+    //Severerror status request
+    public function severerror($message)
+    {
+        return response()->json(['error' => $message], 500);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\SignupController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+Route::group(['namespace' => 'Api'], function () {
+
+    /**
+     * USER ROUTES
+     */
+    Route::group(['prefix' => 'user'], function () {
+
+    //Register a User
+        Route::post('sign-up', [SignupController::class, 'create']);
+    });
+    //---------
 });
